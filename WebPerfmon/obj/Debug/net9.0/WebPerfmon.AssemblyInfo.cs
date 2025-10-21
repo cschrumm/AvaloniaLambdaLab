32efa8f9e6d71471528d4fdcc36430df5da60985
cschrumm <cschrumm@gmail.com>
2025-10-20 20:08:07 -0400

    Working on fixing bugs.

--- WebPerfmon/obj/Debug/net9.0/WebPerfmon.AssemblyInfo.cs
+++ WebPerfmon/obj/Debug/net9.0/WebPerfmon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebPerfmon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd5c3fa58f66f1501f99533eb9b2d8d2c53e7af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52775fc451b57c6ee63a8296684cca97d95e20c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebPerfmon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebPerfmon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
